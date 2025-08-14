8801a1f656ea68a8017e121e8dec39dc3b9a8d75
luckylee524 <dbleecomkr@naver.com>
2025-08-14 13:22:36 +0900

    Fix: #12 domain add

--- markets/views.py
+++ markets/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from django.views.decorators.http import require_http_methods
 from .models import *
+from .serializers import *
 
 # Create your views here.
 
